8f88b077003dcc2a24055560af74a2f383555340
chandan tiwari <chandan@noapp.io>
2023-05-22 11:58:47 +0530

    minor changes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,33 +1,16 @@
 import React from 'react';
-import axios from 'axios';
-
-// Create an instance of Axios with the provided configuration
-const instance = axios.create({
-  baseURL: 'http://localhost:3001/api/',
-  timeout: 5000,
-});
-
-class App extends React.Component {
-  handleClick = async (delay) => {
-    try {
-      const response = await instance.post('delay', { delay });
-      console.log(response.data.message);
-    } catch (error) {
-      console.error('Timeout error:', error);
-    }
-  };
-
-  render() {
-    return (
-      <div style={{display:'flex',height:'auto',width:'auto'}}>
-        <button onClick={() => this.handleClick(1)} style={{display:'flex',height:'80px',width:'165px'}}>1 Second</button>
-        <button onClick={() => this.handleClick(2)} style={{display:'flex',height:'80px',width:'165px'}}>2 Seconds</button>
-        <button onClick={() => this.handleClick(3)} style={{display:'flex',height:'80px',width:'165px'}}>3 Seconds</button>
-        <button onClick={() => this.handleClick(4)} style={{display:'flex',height:'80px',width:'165px'}}>4 Seconds</button>
-        <button onClick={() => this.handleClick(5)} style={{display:'flex',height:'80px',width:'165px'}}>5 Seconds</button>
-      </div>
-    );
-  }
+// import axios from "axios"
+const App = () => {
+  return (
+    <div style={{ display: 'flex', height: 'auto', width: 'auto' }}>
+      <button style={{ display: 'flex', height: '80px', width: '165px' }}>1 Second</button>
+      <button style={{ display: 'flex', height: '80px', width: '165px' }}>2 Seconds</button>
+      <button style={{ display: 'flex', height: '80px', width: '165px' }}>3 Seconds</button>
+      <button style={{ display: 'flex', height: '80px', width: '165px' }}>4 Seconds</button>
+      <button style={{ display: 'flex', height: '80px', width: '165px' }}>5 Seconds</button>
+    </div>
+  )
 }
 
-export default App;
+export default App
+
